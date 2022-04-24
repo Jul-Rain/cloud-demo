49997b33af8816694950ea5dbb388696219ac4ce
yan <1015574542@qq.com>
2022-04-24 18:31:18 +0800

    master-abc

--- cloud-demo/cloud-demo/order-service/src/test/java/com/yan/test.java
+++ cloud-demo/cloud-demo/order-service/src/test/java/com/yan/test.java
@@ -2,10 +2,6 @@ package com.yan;
 
 public class test {
     public static void main(String[] args) {
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
+        System.out.println("abc1");
     }
 }
