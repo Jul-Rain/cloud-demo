24b4b6051052bf2246b64230293a619405f567a7
Jul-Rain <96642215+Jul-Rain@users.noreply.github.com>
2022-04-24 17:52:53 +0800

    Update test.java

--- cloud-demo/cloud-demo/order-service/src/test/java/com/yan/test.java
+++ cloud-demo/cloud-demo/order-service/src/test/java/com/yan/test.java
@@ -3,10 +3,5 @@ package com.yan;
 public class test {
     public static void main(String[] args) {
         System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
-        System.out.println("abc");
     }
 }
